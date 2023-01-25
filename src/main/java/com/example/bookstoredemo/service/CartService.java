095cff6478276ac9781a9c339fddb2b4724c9b7f
sawmeji <sawmejisan@gmail.com>
2023-01-25 15:52:15 +0630

    5th commit

---
+++ src/main/java/com/example/bookstoredemo/service/CartService.java
@@ -0,0 +1,47 @@
+package com.example.bookstoredemo.service;
+
+import com.example.bookstoredemo.dao.BookDao;
+import com.example.bookstoredemo.ds.BookDto;
+import com.example.bookstoredemo.ds.CartBean;
+import com.example.bookstoredemo.entity.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class CartService {
+    @Autowired
+    private CartBean cartBean;
+    @Autowired
+    private BookDao bookDao;
+
+    public void addToCart(int id) {
+        cartBean.addToCart(toDto(bookDao.findById(id).get()));
+    }
+
+    public Set<BookDto> listCart(){
+        return cartBean.listAllCart();
+    }
+
+    public BookDto toDto(Book book) {
+        return new BookDto(
+                book.getId(),
+                book.getTitle(),
+                book.getYearPublished(),
+                book.getPublisher(),
+                book.getPrice(),
+                book.getQuantity(),
+                book.getGenre(),
+                book.getImgUrl(),
+                book.getDescription(),
+                book.getCategory(),
+                book.getAuthor()
+        );
+    }
+
+    public void removeFromCart(BookDto bookDto) {
+        cartBean.removeBook(bookDto);
+    }
+}
